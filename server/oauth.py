8385436d310a1f9b11b8126869c77aa5d5f525d1
FelipeCardos <50990842+FelipeCardos@users.noreply.github.com>
2022-05-20 17:54:37 +0100

    j

--- server/oauth.py
+++
@@ -1,5 +0,0 @@
-class Oauth2Spotify:
-    def __init__(self, client_id, client_secret,redirect_uri) -> None:
-        self.client_id = input("Client ID: ")
-        self.client_secret = input("Client Secret: ")
-    
